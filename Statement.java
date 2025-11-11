bfea9580b1f00996a432f48c9fb2f6bcb73ad9f3
anthonytd2 <avvsilva@minha.fag.edu.br>
2025-11-11 14:46:03 -0300

    Commit 18

--- Statement.java
+++ Statement.java
@@ -1,6 +1,20 @@
 import java.util.Enumeration;
 
 public abstract class Statement {
+
+    public String value(Customer aCustomer) {
+        Enumeration rentals = aCustomer.getRentals();
+        String result = headerString(aCustomer);
+        while (rentals.hasMoreElements()) {
+            Rental each = (Rental) rentals.nextElement();
+            //show figures for this rental
+            result += eachRentalString(each);
+        }
+        //add footer lines
+        result += footerString(aCustomer);
+        return result;
+    }
+
     public abstract String headerString(Customer aCustomer);
     public abstract String eachRentalString(Rental aRental);
     public abstract String footerString(Customer aCustomer);
